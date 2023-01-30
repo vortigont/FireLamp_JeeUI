2d5fa1e82b1b1732d72c1ca39fa75a67db33e028
vortigont <gpm@hotplug.ru>
2023-01-30 14:49:57 +0900

    Update Install.bat
    convert to UTF8
    set variables
    get rid of wget.exe calls

--- Install.bat
+++ Install.bat
@@ -3,8 +3,18 @@ SETLOCAL EnableDelayedExpansion
 set workdir=%~dp0
 PATH=%PATH%;%workdir%;%USERPROFILE%\.platformio\penv\Scripts;
 echo off
-@chcp 1251>nul
+rem set the code page to UTF8
+chcp 65001 >nul
+rem @chcp 1251>nul
 mode con: cols=88 lines=40
+
+rem VARIABLES
+set "lamprepo=https://github.com/vortigont/FireLamp_JeeUI.git"
+set "pythondistro=https://www.python.org/ftp/python/3.10.4/python-3.10.4.exe"
+set "pythondistro64=https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe"
+set "gitdistro=https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-32-bit.exe"
+set "gitdistro64=https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-64-bit.exe"
+
 cls
 
 Echo  Attention. For install Python, run this script "As Administrator". 
@@ -13,25 +23,25 @@ Echo  .
 
 :m1
 Echo  #------------------------------------------#-----------------------------------------# 
-Echo  *                  Commands                *                  Команды                * 
+Echo  *                  Commands                *                  РљРѕРјР°РЅРґС‹                * 
 Echo  #------------------------------------------#-----------------------------------------# 
-Echo  *             Install tools                *          Установка инструментов         * 
+Echo  *             Install tools                *          РЈСЃС‚Р°РЅРѕРІРєР° РёРЅСЃС‚СЂСѓРјРµРЅС‚РѕРІ         * 
 Echo  #------------------------------------------#-----------------------------------------#
-Echo  *  Install Python          (step 1)      1 *  Установить Python             (Шаг 1)  * 
-Echo  *  Install Platformio Core (step 2)      2 *  Установить PIO Core           (Шаг 2)  * 
-Echo  *  Install Git for Windows (step 3)      3 *  Установить Git                (Шаг 3)  * 
-Echo  *  Get firmware repository (step 4)      4 *  Получить репозиторий прошивки (Шаг 4)  *
+Echo  *  Install Python          (step 1)      1 *  РЈСЃС‚Р°РЅРѕРІРёС‚СЊ Python             (РЁР°Рі 1)  * 
+Echo  *  Install Platformio Core (step 2)      2 *  РЈСЃС‚Р°РЅРѕРІРёС‚СЊ PIO Core           (РЁР°Рі 2)  * 
+Echo  *  Install Git for Windows (step 3)      3 *  РЈСЃС‚Р°РЅРѕРІРёС‚СЊ Git                (РЁР°Рі 3)  * 
+Echo  *  Get firmware repository (step 4)      4 *  РџРѕР»СѓС‡РёС‚СЊ СЂРµРїРѕР·РёС‚РѕСЂРёР№ РїСЂРѕС€РёРІРєРё (РЁР°Рі 4)  *
 Echo  #------------------------------------------#-----------------------------------------# 
-Echo  *  Remove Platformio installation        R *  Удалить установку Платформио           * 
+Echo  *  Remove Platformio installation        R *  РЈРґР°Р»РёС‚СЊ СѓСЃС‚Р°РЅРѕРІРєСѓ РџР»Р°С‚С„РѕСЂРјРёРѕ           * 
 Echo  #----------------------------------------#-#-----------------------------------------#
 Echo.
-Set /p choice="Your choice (Ваш выбор): "
+Set /p choice="Your choice (Р’Р°С€ РІС‹Р±РѕСЂ): "
 
 if "%choice%"=="1" (
 	if not exist "%systemdrive%\Program Files (x86)" (
-		%workdir%\resources\wget https://www.python.org/ftp/python/3.10.4/python-3.10.4.exe -O "%TMP%\python.exe"
+		CALL DOWNLOAD_FILE %pythondistro% "%TMP%\python.exe"
 	) else (
-		%workdir%\resources\wget https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe -O "%TMP%\python.exe"
+		CALL DOWNLOAD_FILE %pythondistro%  "%TMP%\python.exe"
 	)
 	
 	%TMP%\python.exe /passive InstallAllUsers=0 PrependPath=1 Include_pip=1 Include_launcher=1 AssociateFiles=1 TargetDir=%USERPROFILE%\Python
@@ -53,16 +63,16 @@ if "%choice%"=="1" (
 )
 
 if "%choice%"=="2" (
-	%workdir%\resources\wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O %workdir%\get-platformio.py
+	CALL DOWNLOAD_FILE https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O %workdir%\get-platformio.py
 	python %workdir%\get-platformio.py
 	del %workdir%\get-platformio.py
 )
 
 if "%choice%"=="3" (
 	if not exist "%systemdrive%\Program Files (x86)" (
-		%workdir%\resources\wget https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-32-bit.exe -O %TMP%\git.exe
+		CALL DOWNLOAD_FILE %gitdistro% %TMP%\git.exe
 	) else (
-		%workdir%\resources\wget https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe -O %TMP%\git.exe
+		CALL DOWNLOAD_FILE %gitdistro64% %TMP%\git.exe
 	)
 	
 	%TMP%\git.exe /SILENT
@@ -70,7 +80,7 @@ if "%choice%"=="3" (
 )
 
 if "%choice%"=="4" (
-	Set /p diskInstal="Enter a drive letter C,D etc. (Введите букву диска C,D и т.п): "
+	Set /p diskInstal="Enter a drive letter C,D etc. (Р’РІРµРґРёС‚Рµ Р±СѓРєРІСѓ РґРёСЃРєР° C,D Рё С‚.Рї): "
 	rem echo  test !%diskInstal!
 	if not exist "!diskInstal!:\" (
 		echo Disk letter is wrong!
@@ -82,7 +92,7 @@ if "%choice%"=="4" (
 		echo "Firmware repo wil be instaled on disk !diskInstal!:"
 		!diskInstal!:
 		if exist "!diskInstal!:\FireLamp_JeeUI_original_int" (rmdir /S /Q !diskInstal!:\FireLamp_JeeUI_original_int)
-		"%ProgramFiles%\Git\bin\git" clone https://github.com/andy040670/FireLamp_JeeUI_original_int.git
+		"%ProgramFiles%\Git\bin\git" clone %lamprepo%
 		start .\FireLamp_JeeUI_original_int
 	)
 )
@@ -98,3 +108,10 @@ if "%choice%"=="1" (
 )
 cls
 goto m1
+
+
+rem FUNCTIONS SECTION
+
+:DOWNLOAD_FILE
+    powershell -Command "Invoke-WebRequest %1 -OutFile %2"
+GOTO :EOF
