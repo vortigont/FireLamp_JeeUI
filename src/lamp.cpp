ddaec3b69c02c22764437ff44b14ceb8aed34b8f
Emil Muratov <gpm@hotplug.ru>
2023-05-23 15:12:52 +0900

    remove worker run() on effect switch
    an issue if effect tries to clear framebuffer on initialization is caused
    by always-enabled copy to sledsbuff before each worker run
    so, we should not execute effect run on switch, but let it do it's job in proper order
    
    closes #9

--- src/lamp.cpp
+++ src/lamp.cpp
@@ -1023,9 +1023,8 @@ void LAMP::switcheffect(EFFSWITCH action, bool fade, uint16_t effnb, bool skip) 
     LOG(printf_P, PSTR("switcheffect() postfade act=%d, fade=%d, effnb=%d\n"), action, fade, effnb ? effnb : effects.getSelected());
   }
 
-  //LOG(printf_P,PSTR(">>>>>>>>>>>isEffClearing==%d\n"),isEffClearing);
   if(flags.isEffClearing || !effects.getEn()){ // для EFF_NONE или для случая когда включена опция - чистим матрицу
-    FastLED.clear();
+    mx.clear();
     FastLED.show();
   }
 
@@ -1069,7 +1068,6 @@ void LAMP::switcheffect(EFFSWITCH action, bool fade, uint16_t effnb, bool skip) 
 
   // отрисовать текущий эффект (только если лампа включена, иначе бессмысленно)
   if(effects.worker && flags.ONflag && !lampState.isEffectsDisabledUntilText){
-    effects.worker->run();
     if(!sledsbuff){ // todo: WHY we need this clone here???
       sledsbuff = new LedFB(mx);  // clone existing frambuffer
     }
