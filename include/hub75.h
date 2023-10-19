8437c6cbc08062f804c16f4f2cdc90be669d79f0
Emil Muratov <gpm@hotplug.ru>
2023-10-19 11:45:55 +0900

    update default hub75 pinout

--- include/hub75.h
+++ include/hub75.h
@@ -2,17 +2,17 @@
 
 // size is statically set to 64x32
 // default gpios https://github.com/vortigont/FireLamp_JeeUI/wiki/%D0%9D%D0%B0%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2%D1%8B%D0%B2%D0%BE%D0%B4%D0%BE%D0%B2
-#define R1  18
+#define R1   0
 #define G1  19
-#define BL1 20
-#define R2  23
-#define G2  24
+#define BL1  2
+#define R2  12
+#define G2   4
 #define BL2 25
 #define CH_A 26
 #define CH_B 27
-#define CH_C 28
-#define CH_D 29
+#define CH_C 23
+#define CH_D 18
 #define CH_E -1 // assign to any available pin if using two panels or 64x64 panels with 1/32 scan
-#define CLK  31
+#define CLK  13
 #define LAT  32
 #define OE   33
