8e93c586f457c1ed177d92a8234fb3754d3fd44e
Emil Muratov <gpm@hotplug.ru>
2024-02-02 15:19:22 +0900

    reworked LOG macros
    updated macro will allow to set severity level and component tag

--- src/log.h
+++ src/log.h
@@ -3,11 +3,45 @@ LOG macro will enable/disable logs to serial depending on LAMP_DEBUG build-time 
 */
 #pragma once
 
+#ifndef LAMP_DEBUG_PORT
+#define LAMP_DEBUG_PORT Serial
+#endif
+
+// undef possible LOG from EmbUI
 #if defined(LOG)
   #undef LOG
 #endif
-#if defined(LAMP_DEBUG)
-	#define LOG(func, ...) Serial.func(__VA_ARGS__)
+
+#if defined(LAMP_DEBUG_LEVEL) && LAMP_DEBUG_LEVEL == 5
+	#define LOGV(tag, func, ...) LAMP_DEBUG_PORT.print(tag); LAMP_DEBUG_PORT.print(" V: "); LAMP_DEBUG_PORT.func(__VA_ARGS__)
 #else
-	#define LOG(func, ...) ;
+	#define LOGV(...)
+#endif
+
+#if defined(LAMP_DEBUG_LEVEL) && LAMP_DEBUG_LEVEL > 3
+	#define LOGD(tag, func, ...) LAMP_DEBUG_PORT.print(tag); LAMP_DEBUG_PORT.print(" D: "); LAMP_DEBUG_PORT.func(__VA_ARGS__)
+#else
+	#define LOGD(...)
+#endif
+
+#if defined(LAMP_DEBUG_LEVEL) && LAMP_DEBUG_LEVEL > 2
+	#define LOGI(tag, func, ...) LAMP_DEBUG_PORT.print(tag); LAMP_DEBUG_PORT.print(" I: "); LAMP_DEBUG_PORT.func(__VA_ARGS__)
+	// compat macro
+	#define LOG(func, ...) LAMP_DEBUG_PORT.func(__VA_ARGS__)
+#else
+	#define LOGI(...)
+	// compat macro
+	#define LOG(...)
+#endif
+
+#if defined(LAMP_DEBUG_LEVEL) && LAMP_DEBUG_LEVEL > 1
+	#define LOGW(tag, func, ...) LAMP_DEBUG_PORT.print(tag); LAMP_DEBUG_PORT.print(" W: "); LAMP_DEBUG_PORT.func(__VA_ARGS__)
+#else
+	#define LOGW(...)
+#endif
+
+#if defined(LAMP_DEBUG_LEVEL) && LAMP_DEBUG_LEVEL == 1
+	#define LOGE(tag, func, ...) LAMP_DEBUG_PORT.print(tag); LAMP_DEBUG_PORT.print(" E: "); LAMP_DEBUG_PORT.func(__VA_ARGS__)
+#else
+	#define LOGE(...)
 #endif
